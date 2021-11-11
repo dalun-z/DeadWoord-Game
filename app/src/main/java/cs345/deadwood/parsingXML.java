6b8faba88998592e81fa808bb0048b67ab69248b
dalunzhang1994 <zhangd2@wwu.edu>
2021-11-10 20:26:38 -0800

    completed version
    Successfully parsing the XML file, maybe need to store the data to somewhere

--- app/src/main/java/cs345/deadwood/parsingXML.java
+++ app/src/main/java/cs345/deadwood/parsingXML.java
@@ -5,6 +5,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -52,19 +53,53 @@ public class parsingXML{
         NodeList cards = root.getElementsByTagName("card");
 
         for(int i = 0; i < cards.getLength(); i++){
-            System.out.println("Cards : " + (i+1));
 
             Node card = cards.item(i);
             String cardName = card.getAttributes().getNamedItem("name").getNodeValue();
             System.out.println("Card name is : " + cardName);
 
-            NodeList child = card.getChildNodes();
+            String imgFileName = card.getAttributes().getNamedItem("img").getNodeValue();
+            System.out.println("Card img file name is : " + imgFileName);
 
-            for(int j = 0; j < child.getLength(); j++){
-                Node sub = child.item(j);
+            String cardBudget = card.getAttributes().getNamedItem("budget").getNodeValue();
+            System.out.println("Card budget is : " + cardBudget);
 
-                if()
+            NodeList part = card.getChildNodes();
+
+            for(int j = 0; j < part.getLength(); j++){
+                Node parts = part.item(j);
+
+                // ByTagName (part, area, etc)
+                if("scene".equals(parts.getNodeName())){
+                    String sceneNum = parts.getAttributes().getNamedItem("number").getNodeValue();
+                    System.out.println("Scene number is : " + sceneNum);
+                    String sceneTxt = parts.getTextContent();
+                    System.out.println("Scene describtion : " + sceneTxt);
+                }
+                
+                if("part".equals(parts.getNodeName())){
+                    String partName = parts.getAttributes().getNamedItem("name").getNodeValue();
+                    System.out.println("Part name : " + partName);
+                    String level = parts.getAttributes().getNamedItem("level").getNodeValue();
+                    System.out.println("Level is : " + level);
+
+                }
+
+                NodeList partInfos = parts.getChildNodes();
+                for(int k = 0; k < partInfos.getLength(); k++){
+
+                    Node partInfo = partInfos.item(k);
+                    if("area".equals(partInfo.getNodeName())){
+                        String x = partInfo.getAttributes().getNamedItem("x").getNodeValue();
+                        String y = partInfo.getAttributes().getNamedItem("y").getNodeValue();
+                        String h = partInfo.getAttributes().getNamedItem("h").getNodeValue();
+                        String w = partInfo.getAttributes().getNamedItem("w").getNodeValue();
+                        System.out.print("Location coordinates : ");
+                        System.out.println("x = " + x + ", y = " + y + ", h = " + h + ", w = " + w);
+                    }
+                }
             }
+            System.out.println("\n");
         }
     }
 }
